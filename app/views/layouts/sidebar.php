47dee58bfc5bbda183f1b6a9e590918ca6cf06d0
Tri <Tringuyendinh230@gmail.com>
2025-12-14 10:13:24 +0700

    thêm header+footer+sidebar

---
+++ app/views/layouts/sidebar.php
@@ -0,0 +1,29 @@
+<div class="d-flex">
+    <div class="bg-primary text-white p-3" style="width:250px; min-height:100vh;">
+        <h5 class="text-center mb-4">KHO GIA DỤNG</h5>
+        <ul class="nav flex-column">
+            <li class="nav-item">
+                <a class="nav-link text-white" href="index.php?controller=home">Trang chủ</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link text-white" href="index.php?controller=product">Sản phẩm</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link text-white" href="index.php?controller=import">Nhập kho</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link text-white" href="index.php?controller=export">Xuất kho</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link text-white" href="index.php?controller=inventory">Tồn kho</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link text-white" href="index.php?controller=report">Báo cáo</a>
+            </li>
+            <li class="nav-item mt-3">
+                <a class="nav-link text-warning" href="index.php?controller=auth&action=logout">Đăng xuất</a>
+            </li>
+        </ul>
+    </div>
+
+    <div class="flex-grow-1 p-4">
